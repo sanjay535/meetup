5e5129de0e30db3c3e7c3b0818afcee1a7fa4956
Sanjay Gautam <sanjaykumargautam535@gmail.com>
2023-04-29 12:43:51 +0530

    mongodb working setup

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,20 +1,28 @@
 /* /api/new-meetup */
-import {MongoClient} from 'mongodb';
+import { MongoClient } from 'mongodb';
 
-async function handler(req, res){
-    if(req.method==='POST'){
-        // it run if /api/new-meetup have POST request
-        const data=req.body;
-        // const {title, image, address, description}=data;
-        const uri = "mongodb+srv://sanjay:sanjay535@cluster0.s8i0unm.mongodb.net/meetups?retryWrites=true&w=majority";
-       const client=await MongoClient.connect(uri);
-       const db=client.db();
-       const meetupCollection=db.collection('meetups');
-       const result=  meetupCollection.insertOne(data);
-       console.log(result);
-       client.close();
-       res.status(201).json({message:'Meetup inserted'})
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    // it run if /api/new-meetup have POST request
+    console.log('req.body=', req.body);
+    const data = req.body;
+    // const {title, image, address, description}=data;
+    const uri =
+      'mongodb+srv://sanjay:gautam535@cluster0.s8i0unm.mongodb.net/meetup?retryWrites=true&w=majority';
+    try {
+      const client = new MongoClient(uri);
+      await client.connect();
+      const db = client.db();
+      const meetupCollection = db.collection('meetups');
+      const result = await meetupCollection.insertOne(data);
+      console.log(result);
+      client.close();
+      res.status(201).json({ message: 'Meetup inserted' });
+    } catch (error) {
+      console.log(`Error=`, error);
     }
+    
+  }
 }
 
 export default handler;
