04cf9c412d5b2bbefb95ad4c1ffac6499bede816
Sanjay Gautam <sanjaykumargautam535@gmail.com>
2023-04-29 18:31:09 +0530

    trying to fix refresh issue 1

--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,52 @@
-import Head from "next/head";
-import { Fragment } from "react";
-import { MongoClient } from "mongodb";
-import MeetupList from "../components/meetups/MeetupList";
+import Head from 'next/head';
+import { Fragment } from 'react';
+import { MongoClient } from 'mongodb';
+import MeetupList from '../components/meetups/MeetupList';
 
-
-function HomePage({meetups}){
-    return  (
-        <Fragment>
-        <Head>
-            <title>React meetups</title>
-            <meta name="description" content="It is React meetups application"/>
-        </Head>
-    <MeetupList meetups={meetups}/>
-    </Fragment>);
+function HomePage({ meetups }) {
+  return (
+    <Fragment>
+      <Head>
+        <title>React meetups</title>
+        <meta name='description' content='It is React meetups application' />
+      </Head>
+      <MeetupList meetups={meetups} />
+    </Fragment>
+  );
 }
 
 // this function runs during build
-export async function getStaticProps(){
-    // fetch data from API
-    // fetch can also works in server side code as well
+export async function getStaticProps() {
+  // fetch data from API
+  // fetch can also works in server side code as well
 
+  const uri =
+    'mongodb+srv://sanjay:gautam535@cluster0.s8i0unm.mongodb.net/meetup?retryWrites=true&w=majority';
+  const client = new MongoClient(uri);
 
-    const uri ='mongodb+srv://sanjay:gautam535@cluster0.s8i0unm.mongodb.net/meetup?retryWrites=true&w=majority';
-    const client = new MongoClient(uri);
-    let meetups;
-    try {
-      await client.connect();
-      const db = client.db();
-      const meetupCollection = db.collection('meetups');
-      const meetupsList = await meetupCollection.find().toArray();
-         meetups=meetupsList.map(item=>({
-         id:item._id.toString(),
-         title:item.title,
-         image:item.image,
-         address:item.address,
-         description:item.description
-        }))
-    } catch (error) {
-      console.log(`Error=`, error);
-    }
-    finally{
-        await client.close();
-    }
-
+  try {
+    await client.connect();
+    const db = client.db();
+    const meetupCollection = db.collection('meetups');
+    const meetupsList = await meetupCollection.find().toArray();
+    const meetups = meetupsList.map((item) => ({
+      id: item._id.toString(),
+      title: item.title,
+      image: item.image,
+      address: item.address,
+      description: item.description,
+    }));
     return {
-        props:{
-            meetups:meetups
-        },
-        revalidate:1
-    } 
-   
+      props: {
+        meetups: meetups,
+      },
+      revalidate: 1,
+    };
+  } catch (error) {
+    console.log(`Error=`, error);
+  } finally {
+    await client.close();
+  }
 }
 
 export default HomePage;
